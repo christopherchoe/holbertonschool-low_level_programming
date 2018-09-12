b7fd0e1c6981c2ff33c1ec237f81fe346a1d7294
christopherchoe <chchchoe@gmail.com>
2018-09-12 22:18:50 +0000

    pt 9 commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char a[62] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
+	write(2,a,sizeof(a));
+	return (1);
+}
