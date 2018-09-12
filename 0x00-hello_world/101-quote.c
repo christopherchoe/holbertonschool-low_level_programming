a6997c309602e90ff10ba7af014519ebd41d4abd
christopherchoe <chchchoe@gmail.com>
2018-09-12 22:24:51 +0000

    fixed style issues pt 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-	char a[62] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
-	write(2,a,sizeof(a));
+	char a[62] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, a, sizeof(a));
 	return (1);
 }
