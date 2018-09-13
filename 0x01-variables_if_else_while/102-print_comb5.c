d31bcea585a0c32f5a6be34623a614ab85ee8547
christopherchoe <chchchoe@gmail.com>
2018-09-13 22:36:46 +0000

    pt 13 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,26 +11,30 @@ int main(void)
 	int n;
 	int o;
 	int p;
+	int q;
 
 	for (m = 48; m < 58; m++)
 	{
 		for (n = 48; n < 58; n++)
 		{
+			q = n + 1;
 			for (o = m; o < 58; o++)
 			{
-				for (p = n + 1; p < 58; p++)
+				for (p = q; p < 58; p++)
 				{
-					if (n != 58 || m != 57 || o != 58 || p != 58)
+					putchar(m);
+					putchar(n);
+					putchar(32);
+					putchar(o);
+					putchar(p);
+					if (m != 57 || n != 56 || o != 57 || p != 57)
 					{
-						putchar(m);
-						putchar(n);
-						putchar(32);
-						putchar(o);
-						putchar(p);
 						putchar(44);
 						putchar(32);
 					}
+					q++;
 				}
+				q = 48;
 			}
 		}
 	}
