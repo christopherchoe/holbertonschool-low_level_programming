e972431389765bef5a8ca7370347491d40899daf
christopherchoe <chchchoe@gmail.com>
2018-09-17 22:45:14 +0000

    part 11 commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from a given digit to 98 in natural numbers.
+ *
+ * n: integer to start from.
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		else
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("%d\n", n);
+}
