c4ed79a06e379382112c3af080ed53881bccfb4f
christopherchoe <chchchoe@gmail.com>
2018-09-18 17:45:43 +0000

    pt 16 commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 98 fibonacci numbers starting with 1.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	double x = 1, y = 2, a;
+	int z;
+
+	printf("%.0f, %.0f, ", x, y);
+	for (z = 0; z < 96; z++)
+	{
+		a = y;
+		y += x;
+		x = a;
+		printf("%.0f", y);
+		if (z != 95)
+			printf(", ");
+		else
+			putchar('\n');
+	}
+	return (0);
+}
