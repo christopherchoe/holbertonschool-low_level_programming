c0d780120cc19d6a27c2f0ce97b609f5a41eef1b
christopherchoe <chchchoe@gmail.com>
2018-09-18 16:18:39 +0000

    fixed number in pt 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,9 +10,9 @@ int main(void)
 	double l;
 	int i, k;
 
-	for (i = 0; i < 1000; i += 3)
+	for (i = 0; i < 1024; i += 3)
 		l += i;
-	for (k = 0; k < 1000; k += 5)
+	for (k = 0; k < 1024; k += 5)
 	{
 		if ((k % 3) != 0)
 			l += k;
