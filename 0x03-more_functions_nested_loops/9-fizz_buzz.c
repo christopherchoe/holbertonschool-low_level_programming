b63cc36f23e495b62a34c2c9b6ae27440d454dee
christopherchoe <chchchoe@gmail.com>
2018-09-20 08:36:18 +0000

    delete compiled file and new commit pt 9

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,26 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
+ * main - Entry point, print fizzbuzz integers
  *
- *
- *
+ * Return: Always 0 (Success)
  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz");
+		if ((i % 5) == 0)
+			printf("Buzz");
+		if (((i % 3) != 0) && ((i % 5) != 0))
+			printf("%d", i);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
 
