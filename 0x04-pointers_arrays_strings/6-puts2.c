b40e7ab7f8f7fa8112a13375189e82fe7fc5ed7a
christopherchoe <chchchoe@gmail.com>
2018-09-25 16:20:53 +0000

    pt 6 commit

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -4,16 +4,18 @@
 /**
  * puts2 - prints one character of two
  *
- * @str: string to print one character of
+ * @star: string to print one character of
  * Return: void
  */
 void puts2(char *star)
 {
 	int i;
 
-	for (i = 0; *(star + i) != '\0'; i += 2)
+	for (i = 0; *(star + i) != '\0'; i++)
 	{
 		_putchar(*(star + i));
+		if (*(star + i + 1) != '\0')
+			i++;
 	}
 	_putchar('\n');
 }
