ea31c893760870c9213a17f1d4fd260e40d53226
christopherchoe <chchchoe@gmail.com>
2018-09-27 20:14:46 +0000

    part 3 fix

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int str1 = 0, str2 = 0;
-
 	while (*s1 != '\0' || *s2 != '\0')
 	{
-		if (*s1 != '\0')
-			str1 += *s1++;
-		if (*s2 != '\0')
-			str2 += *s2++;
+		if (*s1 - *s2 != 0)
+			return (*s1 - *s2);
+		if (*(s1) != '\0')
+			s1++;
+		if (*(s2) != '\0')
+			s2++;
 	}
-	if ((str1 - str2) > 0)
-		return (15);
-	else if ((str1 - str2) < 0)
-		return (-15);
-	else
-		return (0);
+	return (0);
 }
