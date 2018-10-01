3202e7802416ce478fc550494df38dc87aca3907
christopherchoe <chchchoe@gmail.com>
2018-10-01 11:55:00 +0000

    second test pt 11

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "holberton.h"
 
+int addi(char *n2, char *r, int, int, int, int);
+
 /**
  * *infinite_add - adds two numbers in a buffer sized to size_r
  * only positive numbers or 0 and only digits in the string. The
@@ -16,7 +18,7 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int n1_len, n2_len;
-	int n, i, max, temp;
+	int i, max, flag;
 
 	for (n1_len = 0; *(n1 + n1_len); n1_len++)
 		;
@@ -33,7 +35,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	n2_len -= max;
 	for (i = 0; i < max - 1; i++)
 	{
-		n = i;
 		printf("r is: %c \n n1 is: %c \n", *(r+i), *(n1+n1_len));
 		if (n1_len >= 0)
 			*(r + i) = *(n1 + n1_len);
@@ -41,33 +42,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		printf("r is now after step 1: %s\n", r);
 		if (n2_len >= 0)
 		{
-			*(r + i) += *(n2 + n2_len) - '0';
-			while (*(r + n) > '9')
-			{
-				*(r + n) -= 10;
-				if (n > 0)
-				{
-					n--;
-					*(r + n) += 1;
-				}
-				else
-				{
-					if (max + 1 > size_r)
-						return (0);
-					*(r + max) = '\0';
-					printf("testing: %c\n", *(r));
-					temp = *(r);
-					*(r) = '1';
-					for (n = 1; n <= i + 1; n++)
-					{
-						*(r + n) = temp;
-						if (*(r + n + 1) != '\0')
-							temp = *(r + n + 1);
-					}
-					*(r + max) = '\0';
-				}
-			}
-			printf("r is now after step 2: %s\n", r);
+			flag = addi(n2, r, size_r, i, n2_len, max);
+			if (flag == 0)
+				return (0);
+			else if (flag < 0)
+				i++;
 		}
 		n1_len++;
 		n2_len++;
@@ -75,10 +54,39 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	return (r);
 }
 
-int addition(char *n1, char *n2, char *r, int size_r, int i, int n2_len)
+int addi(char *n2, char *r, int size_r, int i, int n2_len, int max)
 {
-	int n;
+	int n, temp;
 
 	n = i;
-	return (0);
+
+	*(r + i) += *(n2 + n2_len) - '0';
+	while (*(r + n) > '9')
+	{
+		*(r + n) -= 10;
+		if (n > 0)
+		{
+			n--;
+			*(r + n) += 1;
+		}
+		else
+		{
+			if (max + 1 > size_r)
+				return (0);
+			*(r + max) = '\0';
+			printf("testing: %c\n", *(r));
+			temp = *(r);
+			*(r) = '1';
+			for (n = 1; n <= i + 1; n++)
+			{
+				*(r + n) = temp;
+				if (*(r + n + 1) != '\0')
+					temp = *(r + n + 1);
+			}
+			*(r + max) = '\0';
+			return (-1);
+		}
+	}
+	printf("r is now after step 2: %s\n", r);
+	return (1);
 }
