ad07a8fe4fe8efef2b12f6544f594c095cb44ac1
christopherchoe <chchchoe@gmail.com>
2018-10-01 20:21:16 +0000

    possible fix

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+	char *null = NULL;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
 			return (s + i);
 	}
-	return (s + i);
+	return (null);
 }
