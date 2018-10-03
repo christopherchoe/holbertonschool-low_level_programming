0cad43535169019e2824bdb6a5e67626d9b8d938
chchchoe <chchchoe@gmail.com>
2018-10-03 20:16:45 +0000

    part 6 commit

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int check_prime(int a, int b);
+
+/**
+ * is_prime_number - checks if int is a prime number
+ *
+ * @n: int to check if prime
+ * Return: int 1 if prime, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		;
+	else if (check_prime(n, n / 2) > 1)
+		return (1);
+	return (0);
+}
+
+/**
+ * check_prime - checks if an int a is divisible by b
+ *
+ * @a: checking divisibility
+ * @b: checking if multiple of a
+ * Return: the int that is divisible
+ */
+int check_prime(int a, int b)
+{
+	if (a == 1)
+		return (1);
+	if (a % b == 0)
+		return (b);
+	else
+		return (check_prime(a, b - 1));
+}
