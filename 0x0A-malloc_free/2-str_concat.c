ef0c6f1420ae17e9487d8863c81fee231137b381
chchchoe <chchchoe@gmail.com>
2018-10-09 19:56:30 +0000

    part 2 null fix

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -13,11 +13,15 @@ char *str_concat(char *s1, char *s2)
 	char *c;
 	int i, k;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		;
 	for (k = 0; *(s2 + k) != '\0'; k++)
 		;
-	c = malloc(i + k - 1);
+	c = malloc(i + k);
 	if (c == 0)
 		return (NULL);
 	for (i = 0; *(s1 + i) != '\0'; i++)
