6be95a0761aa2f1b1c74c8ec0bc3c31565fa6041
chchchoe <chchchoe@gmail.com>
2018-10-11 03:04:34 +0000

    first draft pt 6

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
 /**
  * **strtow - splits a string into words
  *
@@ -11,25 +9,49 @@
  */
 char **strtow(char *str)
 {
+	char **a;
+	int i, k, n, word_count, word_len, current_word;
+
+	word_count = 0;
+	current_word = 0;
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	return (s);
-}
-
-/**
- * word_size - finds size of word
- *
- * @s: string positioned to find word
- * Return: length of word
- */
-int word_size(char *s)
-{
-	int i, k;
-
-	while (*(s + k) == ' ')
-		k++;
-	for (i = 0; *(s + k + i) != ' ' && *(s + k + i) != '\0'; i++)
-		;
-	return (1 + i + (word_size(s + i + k));
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		if (*(str + i) != ' ')
+		{
+			if (*(str + i + 1) == ' ' || *(str + i + 1) == '\0')
+				word_count++;
+		}
+	}
+	a = malloc(sizeof(char *) * (word_count + 1));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		if (*(str + i) != ' ')
+		{
+			for (k = i; *(str + k) != ' '; k++)
+			{
+				if (*(str + k) != '\0')
+					word_len++;
+			}
+			a[current_word] = malloc(sizeof(char) * (word_len + 1));
+			if (a == NULL)
+			{
+				for (; current_word >= 0; current_word--)
+					free(a[current_word]);
+				free(a);
+				return (NULL);
+			}
+			for (n = 0; i < k; i++)
+			{
+				a[current_word][n++] = *(str + i);
+			}
+			a[current_word][n] = '\0';
+			current_word++;
+		}
+	}
+	a[word_count] = NULL;
+	return (a);
 }
