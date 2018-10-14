a5251f0e4da61f7b5214bb39d72b90ae91cdce4b
christopherchoe <chchchoe@gmail.com>
2018-10-14 04:04:47 +0000

    first push realloc clone

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *_realloc - reallocates memory for a pointer
+ *
+ * @ptr: pointer to old memory allocated
+ * @old_size: size of memory before
+ * @new_size: size of memory after
+ * Return: pointer to new memory allocated
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		new = malloc(new_size);
+		return (new);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new = malloc(new_size);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < new_size; i++)
+		new[i] = *((char *)ptr + i);
+	free(ptr);
+	return (new);
+}
+
