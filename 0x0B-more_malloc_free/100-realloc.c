132ea84a5b1f4be9527b5935632b6f88ee7bca45
christopherchoe <chchchoe@gmail.com>
2018-10-14 04:21:28 +0000

    fixed return issue for equal sizes

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i, min;
 
 	if (new_size == old_size)
-		return (NULL);
+		return (ptr);
 	if (ptr == NULL)
 	{
 		new = malloc(new_size);
