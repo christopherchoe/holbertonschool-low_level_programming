6e3e5289c4524335dcccb1efa6e04a3a8c69c53c
chchchoe <chchchoe@gmail.com>
2018-10-15 19:57:53 +0000

    added final header

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -9,12 +9,17 @@
  *
  * Description: a dog structure with information on the dog and owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG */
 
