c2d239ec98a6b199b91b20fa7aeb8acd93d36b32
chchchoe <chchchoe@gmail.com>
2018-10-17 20:32:51 +0000

    tried changing return

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -38,7 +38,9 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 
-	printf("%d\n", (*get_op_func(argv[2]))(a, b));
-	return (0);
+	a = (*get_op_func(argv[2]))(a, b);
+	printf("%d\n", a);
+
+	return (a);
 }
 
