a391a61b15928f0d73535c445aa3d00d2c7721f6
chchchoe <chchchoe@gmail.com>
2018-10-19 19:04:26 +0000

    first commit print nums

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers with separator in between
+ *
+ * @separator: string to print between numbers
+ * @n: count of numbers
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	putchar('\n');
+}
+
