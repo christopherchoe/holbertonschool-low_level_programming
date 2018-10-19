57138bd8cecd53d172f68f433fecd769eaad6faa
chchchoe <chchchoe@gmail.com>
2018-10-19 17:43:44 +0000

    first commit sum function

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns sum of all parameters
+ *
+ * @n: number of parameters
+ * Return: 0 if n is 0, else sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i, sum;
+
+	va_start(nums, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(nums, int);
+	return (sum);
+}
+
