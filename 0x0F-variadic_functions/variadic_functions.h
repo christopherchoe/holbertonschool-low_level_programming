ab67b16b8105de51f7eb8a229e82c252c0080ad6
christopherchoe <chchchoe@gmail.com>
2018-10-20 22:37:00 +0000

    fixed issue with struct description

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -8,6 +8,12 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct typ - struct for char of type and function
+ *
+ * @t: char of data type to print
+ * @type_name: function that prints that type
+ */
 typedef struct typ
 {
 	char t;
