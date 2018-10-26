3c5e43a0f2957537caef56640f96d89db48987f0
christopherchoe <chchchoe@gmail.com>
2018-10-26 00:43:13 +0000

    got rid of main

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -14,13 +14,3 @@ void print_fast(void)
 	printf("I bore my house upon my back!\n");
 }
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
