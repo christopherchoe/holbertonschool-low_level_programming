9e0f0327a7b1fdbcca08203eba9f207cde313639
christopherchoe <chchchoe@gmail.com>
2018-10-30 07:37:37 +0000

    fixing failure case

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+  * print_listint_safe - prints a linked list
+  *
+  * @head: head of list
+  * Return: number of nodes
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t n = 0, i = 0;
+	const listint_t *loop;
+	const listint_t *h;
+
+	h = head;
+	loop = head;
+	while (head)
+	{
+		printf("[%p] %d\n", (void *) head, head->n);
+		head = head->next;
+		while (i < n)
+		{
+			if (head == loop)
+			{
+				printf("-> [%p] %d\n", (void *) head, head->n);
+				exit(98);
+			}
+			loop = loop->next;
+			i++;
+		}
+		loop = h;
+		i = 0;
+		n++;
+	}
+	return (n);
+}
+
--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,6 +39,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 			*head = new_node;
 	}
+	else
+	{
+		return (NULL);
+	}
 	return (new_node);
 }
 
