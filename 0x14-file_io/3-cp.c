5e1b69d7358cc9b12c9450c0153411376187044f
christopherchoe <chchchoe@gmail.com>
2018-11-07 05:18:36 +0000

    fixed closing issues

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void error_file_from(char *, char *);
-void error_file_to(char *, char *);
+void error_file_from(char *, char *, int);
+void error_file_to(char *, char *, int);
 void error_close(int, char *);
 
 /**
@@ -26,27 +26,23 @@ int copy_file(char *file_to, char *file_from)
 
 	from = open(file_from, O_RDONLY);
 	if (from == -1)
-		error_file_from(file_from, buf);
+		error_file_from(file_from, buf, from);
 
 	to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
-		error_file_to(file_to, buf);
+		error_file_to(file_to, buf, to);
 
 	while (re > 0)
 	{
 		re = read(from, buf, 1024);
 		if (re == -1)
-			error_file_from(file_from, buf);
+			error_file_from(file_from, buf, from);
 		wr = write(to, buf, re);
 		if (wr == -1)
-			error_file_to(file_to, buf);
+			error_file_to(file_to, buf, to);
 	}
-	err1 = close(to);
-	if (err1 == -1)
-		error_close(to, buf);
-	err2 = close(from);
-	if (err2 == -1)
-		error_close(from, buf);
+	error_close(to);
+	error_close(from);
 	free(buf);
 	return (0);
 }
@@ -59,20 +55,28 @@ int copy_file(char *file_to, char *file_from)
   */
 void error_close(int fd, char *buf)
 {
+	int err;
+
+	err = close(fd);
 	free(buf);
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-	exit(100);
+	if (err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 
 /**
   * error_file_from - error procedure when issue with file from
   * @file_from: name of file from
   * @buf: buffer to free
+  * @from: fd
   * Return: void
   */
-void error_file_from(char *file_from, char *buf)
+void error_file_from(char *file_from, char *buf, int from)
 {
 	free(buf);
+	error_close(from);
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 	exit(98);
 }
@@ -81,11 +85,13 @@ void error_file_from(char *file_from, char *buf)
   * error_file_to - error procedure when file to cant be created
   * @file_to: name of file to
   * @buf: buffer to free
+  * @to: fd
   * Return: void
   */
-void error_file_to(char *file_to, char *buf)
+void error_file_to(char *file_to, char *buf, int to)
 {
 	free(buf);
+	error_close(to);
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 	exit(99);
 }
