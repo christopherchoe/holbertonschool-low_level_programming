22fd6633eee4eebd6fd9fc682a7eb52dbf85ffc9
christopherchoe <chchchoe@gmail.com>
2018-11-08 21:50:57 +0000

    fixed freeing issue for failure in loop

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -18,33 +18,32 @@ char **strtow(char *str)
 	{
 		if (*(str + i) != ' ')
 			word_count++;
-		while (*(str + i) != ' ' && *(str + i + 1) != '\0')
+		while (*(str + i) != ' ' && *(str + i))
 			i++;
+		if (!*(str + i))
+			break;
 	}
 	a = malloc(sizeof(char *) * (word_count + 1));
 	if (a == NULL)
 		return (NULL);
-	for (i = 0; current_word < word_count; i++)
+	for (i = 0, k = i; current_word < word_count; i++, word_len = 0, k = i)
 	{
-		if (*(str + i) != ' ')
+		while (*(str + k) != ' ' && *(str + k))
 		{
-			for (k = i; *(str + k) != ' '; k++)
-			{
-				if (*(str + k) != '\0')
-					word_len++;
-			}
-			a[current_word] = malloc(sizeof(char) * (word_len + 1));
-			if (a == NULL)
-			{
-				for (; current_word >= 0; current_word--)
-					free(a[current_word]);
-				free(a);
-				return (NULL);
-			}
-			for (n = 0; i < k && *(str + i) != '\0'; i++)
-				a[current_word][n++] = *(str + i);
-			a[current_word++][n] = '\0';
+			k++;
+			word_len++;
 		}
+		a[current_word] = malloc(sizeof(char) * word_len + 1);
+		if (!a[current_word])
+		{
+			while (current_word-- >= 0)
+				free(a[current_word]);
+			free(a);
+			return (NULL);
+		}
+		for (n = 0; i < k; i++, n++)
+			a[current_word][n] = *(str + i);
+		a[current_word++][n] = '\0';
 	}
 	a[word_count] = NULL;
 	return (a);
