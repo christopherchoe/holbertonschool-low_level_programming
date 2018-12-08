e6b2853df08a03d0e81fff4d86268303ca1e5f75
christopherchoe <chchchoe@gmail.com>
2018-12-08 22:09:23 +0000

    added function description

--- 0x16-doubly_linked_lists/4-free_dlistint.c
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+  * free_dlistint - frees a linked list
+  *
+  * @head: first member of list
+  * Return: void
+  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp = NULL;
