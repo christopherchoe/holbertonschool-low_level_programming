19640136d45ae5918f93bd24b8411d45115dabb8
christopherchoe <chchchoe@gmail.com>
2019-01-15 04:29:51 +0000

    initial commit

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 	hash_node_t **array;
+	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
@@ -18,6 +19,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
 	hash_table->size = size;
 	hash_table->array = array;
