620660a77be6a4b017c12693aca839b659773d5d
christopherchoe <chchchoe@gmail.com>
2019-02-03 04:15:57 +0000

    initial commit

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+void swap_list(listint_t *, listint_t *, listint_t **);
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+	listint_t *temp = NULL;
+
+	if (list == NULL && *list == NULL && (*list)->next == NULL)
+		return;
+
+	curr = (*list)->next;
+	prev = curr->prev;
+	while (curr != NULL)
+	{
+		prev = curr->prev;
+		while (prev != NULL && prev->n > curr->n)
+		{
+			swap_list(curr, prev, list);
+			prev = curr->prev;
+		}
+		curr = curr->next;
+	}
+}
+
+void swap_list(listint_t *curr, listint_t *prev, listint_t **head)
+{
+	listint_t *temp1 = curr->next;
+	listint_t *temp2 = prev->prev;
+
+	if (temp1 != NULL)
+		temp1->prev = prev;
+	if (temp2 != NULL)
+		temp2->next = curr;
+	curr->prev = temp2;
+	prev->next = temp1;
+	curr->next = prev;
+	prev->prev = curr;
+	if (*head == prev)
+		*head = curr;
+	print_list(*head);
+}
