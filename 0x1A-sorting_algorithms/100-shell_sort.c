9171c5ce5f82a1255a754ac9295b89784ebb30d9
christopherchoe <chchchoe@gmail.com>
2019-02-07 00:50:13 +0000

    initial commit

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+void insertion_sort(int *, int, int, size_t);
+
+/**
+  * shell_sort - sorts using shell sort and knuths sequence
+  *
+  * @array: array to sort
+  * @size: size of array
+  */
+void shell_sort(int *array, size_t size)
+{
+	int n = 1;
+	int i;
+
+	while (n < (int)size)
+		n = n * 3 + 1;
+	n = (n - 1) / 3;
+
+	while (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			insertion_sort(array, i, n, size);
+		}
+		print_array(array, size);
+		n = (n - 1) / 3;
+	}
+}
+
+/**
+  * insertion_sort - sorts a subarray using insertion sort
+  *
+  * @array: array containing sub array
+  * @start: starting point of sub array
+  * @n: gaps to have between sorting
+  * @size: size of array
+  */
+void insertion_sort(int *array, int start, int n, size_t size)
+{
+	int i, k, temp;
+
+	for (i = start; i < (int)size; i += n)
+	{
+		for (k = i; k > 0 && array[k] < array[k - n]; k = k - n)
+		{
+			temp = array[k];
+			array[k] = array[k - n];
+			array[k - n] = temp;
+		}
+	}
+}
