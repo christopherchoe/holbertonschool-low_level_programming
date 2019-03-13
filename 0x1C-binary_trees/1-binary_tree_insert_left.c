d5c69d1add6a208e646b102ecee4a423411bc392
christopherchoe <chchchoe@gmail.com>
2019-03-13 19:58:01 +0000

    fixed issue of pointer for new node parent

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		new_node->left->parent = new_node;
 	}
 	parent->left = new_node;
 
