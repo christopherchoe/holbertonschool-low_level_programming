17f63eedd2f3bf4f9e401bf34199ae26ef2db718
christopherchoe <chchchoe@gmail.com>
2019-03-16 18:56:51 -0700

    initial commit

---
+++ 0x1C-binary_trees/111-bst_insert.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - insert into bst
+ *
+ * @tree: tree to insert to
+ * @value: value to insert
+ * Return: pointer to created node or NULL on failure
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	
+	if (value == (*tree)->n)
+		return (NULL);
+
+	if (value > (*tree)->n)
+	{
+		if ((*tree)->right != NULL)
+			return (bst_insert(&((*tree)->right), value));
+		(*tree)->right = binary_tree_node(*tree, value);
+		return ((*tree)->right);
+	}
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left != NULL)
+			return (bst_insert(&((*tree)->left), value));
+		(*tree)->left = binary_tree_node(*tree, value);
+		return ((*tree)->left);
+	}
+	return (NULL);
+}
