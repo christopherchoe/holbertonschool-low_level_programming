6f857826f3b907205470cd15a5eca1dde3ff0780
christopherchoe <chchchoe@gmail.com>
2019-04-17 04:56:22 +0000

    updated header with new struct

--- 0x1D-search_alogrithms/search_algos.h
+++ 0x1D-search_alogrithms/search_algos.h
@@ -8,6 +8,23 @@
 #include <math.h>
 
 /**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+/**
  * struct skiplist_s - Singly linked list with an express lane
  *
  * @n: Integer
